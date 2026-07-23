58db02164fd7dc934642e5f944781465963bf147
levantricd <levantricd@gmail.com>
2026-07-23 20:41:47 +0700

    Initial ASP.NET Core MVC project with Entity Framework setup

---
+++ WebDatLichKham/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDatLichKham.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; } = string.Empty;
+
+        [Phone]
+        public string? Phone { get; set; }
+    }
+}
